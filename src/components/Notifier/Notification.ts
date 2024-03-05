8796f0894fc6e2d2322a7333ea6ff9c250f7d4d2
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-03-05 00:56:28 -0300

    feat: Notification

---
+++ src/components/Notifier/Notification.ts
@@ -0,0 +1,72 @@
+import {toast} from 'react-toastify'
+
+export default function Notification(status : string, message : string){
+
+    switch(status){
+        case 'success':
+            toast.success(`${message}`, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            break
+        
+        case 'error':
+            toast.error(`${message}`, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            break
+
+        case 'warning':
+            toast.warning(`${message}`, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            break
+
+        case 'info':
+            toast.info(`${message}`, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            break
+
+        default:
+            toast(`${message}`, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            break
+    }
+
+}
